f16b841e59ad14f64581b49daff304832596f10a
Adrien Thebo <adrien@puppetlabs.com>
2011-09-28 00:16:48 -0700

    Add proper handling of PINGs

--- lib/wrack/callback/ping.rb
+++ lib/wrack/callback/ping.rb
@@ -2,8 +2,9 @@ class Wrack
   module Callback
     class Ping
       def call(connection, msg)
-        if msg =~ /^PING/i
-          connection.write("PONG")
+        if msg =~ /^PING :(.*)/i
+          servers = $1.split(" ")
+          connection.write("PONG #{servers[0]")
         end
       end
     end
