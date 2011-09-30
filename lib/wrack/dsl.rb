c54c5da301caf87b00eaae8bd369fd423e6a223f
Adrien Thebo <adrien@puppetlabs.com>
2011-09-29 22:35:49 -0700

    Rewrote DSL

--- lib/wrack/dsl.rb
+++ lib/wrack/dsl.rb
@@ -3,22 +3,14 @@
 # Because yaml is hard, and DSLs are cheap
 require 'wrack'
 module Wrack
-  class Bot
-    def initialize(&block)
-      @connection = Wrack::Connection.new
-      instance_eval &block
+  class Session
+
+    def self.bot(options = {}, &block)
+      self.new(options).instance_eval &block
     end
 
     def connection(&block)
       yield @connection
-    end
-
-    def connect
-      @connection.connect
-    end
-
-    def register_callback(*args)
-      @connection.register_callback(*args)
     end
   end
 end
