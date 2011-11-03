c029f3c6a63c8048b88fe90c2fb8aa07fa708949
Adrien Thebo <adrien@puppetlabs.com>
2011-11-02 23:26:47 -0700

    Added colorized logging. :>

--- lib/wrack/plugin/logging.rb
+++ lib/wrack/plugin/logging.rb
@@ -5,8 +5,8 @@ module Wrack
       include Wrack::PluginBase
       
       on_initialize do
-        connection.register_callback(self, :write) {|raw| puts "-> #{raw}" }
-        connection.register_callback(self, :read)  {|raw| puts "<- #{raw}" }
+        connection.register_callback(self, :write) {|raw| puts "\033[31m->\033[0m #{raw}" }
+        connection.register_callback(self, :read) {|raw| puts "\033[32m<-\033[0m #{raw}" }
       end
     end
   end
