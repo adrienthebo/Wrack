ce7eee7226a61a4339d3f0ed135520c26a8fa642
Adrien Thebo <adrien@puppetlabs.com>
2011-11-02 17:10:55 -0700

    Refactored the plugin plugin

--- lib/wrack/plugin/plugin.rb
+++ lib/wrack/plugin/plugin.rb
@@ -7,25 +7,39 @@ module Wrack
       include Wrack::PluginBase
       receive do
         restrict :message => /^!plugin/
-
         receive do
           restrict :message => /reload/
-          match do |msg|
-            puts "Reloading all plugins"
-            Wrack::Plugin.loader.reload_known_plugins!
-            bot.reload_plugins!
-          end
+          match { |msg| reload(msg) }
         end
 
         receive do
           restrict :message => /list/
-          match do |msg|
-            privmsg msg.sender, "--- Registered plugins ---"
-            Wrack::Plugin.registered.each do |plugin|
-              privmsg msg.sender, plugin
-            end
-          end
+          match { |msg| list(msg) }
         end
+
+        receive do
+          restrict :message => /destroy\s+(\S+)/
+          match { |msg| destroy(msg) }
+        end
+      end
+
+      def list(msg)
+        privmsg msg.sender, "--- Registered plugins ---"
+        Wrack::Plugin.registered.each do |plugin|
+          privmsg msg.sender, plugin
+        end
+      end
+
+      def reload(msg)
+        privmsg msg.sender, "Reloading all plugins"
+        Wrack::Plugin.loader.reload_known_plugins!
+        bot.reload_plugins!
+      end
+
+      def destroy(msg)
+        msg.message.match /destroy\s+(\S+)/
+        privmsg msg.sender, "Destroying #{$1}"
+        Wrack::Plugin.destroy $1.intern
       end
     end
   end
